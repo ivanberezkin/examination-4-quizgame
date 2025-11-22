b545925e500ad73cf2bf1f5f41308a999598f47b
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2025-11-22 11:27:00 +0100

    Added instance variables

--- src/User.java
+++ src/User.java
@@ -1,4 +1,9 @@
+import java.util.UUID;
+
 public class User {
+
+    private final UUID id;
+    private String username;
 
     public enum UserState {
         CONNECTED,
@@ -7,4 +12,11 @@ public class User {
         FINISHED,
         DISCONNECTED,
     }
+    private UserState state;
+
+    private int score;
+    private int correctAnswers;
+    private int incorrectAnswers;
+    private boolean connected;
+
 }
