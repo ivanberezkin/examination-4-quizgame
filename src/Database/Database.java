ab68adb716a2fbc63d56fe08299103faa7c6661a
ivbe0111 <berezkinivan94@gmail.com>
2025-11-24 12:34:08 +0100

    created Database Class, Moved Questions to Database

---
+++ src/Database/Database.java
@@ -0,0 +1,16 @@
+package Database;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    private final ArrayList<Question> questions = new ArrayList<>();
+
+    Database() {
+
+
+
+
+    }
+
+}
--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -1,12 +1,9 @@
 package GUI;
 
 import javax.swing.*;
-import javax.swing.text.StyledDocument;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 public class GamePanel extends JPanel {
 
