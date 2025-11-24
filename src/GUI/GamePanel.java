e6482662967650afe43730996974974b212624f3
ivbe0111 <berezkinivan94@gmail.com>
2025-11-24 10:55:46 +0100

    created testButton to move to GamePanel

---
+++ src/GUI/GamePanel.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+
+
+
+}
--- src/GUI/MenuPanel.java
+++ src/GUI/MenuPanel.java
@@ -7,12 +7,26 @@ public class MenuPanel extends JPanel {
     JPanel menuPanel = new JPanel();
     JLabel welcomeLabel = new JLabel();
 
+    JButton testButtonToGamePanel = new JButton();
+
     public MenuPanel(String username) {
 
         welcomeLabel.setText("Welcome " + username);
         add(welcomeLabel);
 
 
+
+        //Test knapp bara, för att kunna gå vidare till Game Panel
+        add(testButtonToGamePanel);
+        testButtonToGamePanel.addActionListener(e -> {
+            JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
+            GamePanel gamePanel = new GamePanel();
+            frame.setContentPane(gamePanel);
+            frame.revalidate();
+            frame.repaint();
+        });
+
+        //TODO lämnar till Raymond
 
     }
 
