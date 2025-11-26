0533bcb6f79348ec6d32b9e7f8f06b750fba62a5
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2025-11-26 17:26:54 +0100

    added a comment in clienthandler

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -7,6 +7,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+//The old server
+
 public class ClientHandler extends Thread {
     ObjectOutputStream out;
     ObjectInputStream in;
