1041c574803c14d9457fbc8e2723721b69f6d182
ivbe0111 <berezkinivan94@gmail.com>
2025-11-28 13:20:36 +0100

    created AvatarPanel and added first Avatar

--- src/GUI/SettingsPanel.java
+++ src/GUI/SettingsPanel.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SettingsPanel extends JPanel {
     private JFrame frame;
@@ -11,6 +12,16 @@ public class SettingsPanel extends JPanel {
         this.frame = frame;
         this.previousPanel = previousPanel;
 
+        setLayout(new GridLayout(3,10));
+        JPanel avatarPanel = new JPanel();
+
+        //Scaled avatars
+        ImageIcon avatar1 = scaleAvatarPicture("resources/avatarSmokey.jpg");
+        JButton avatar1Button = new JButton(avatar1);
+
+        avatarPanel.add(avatar1Button);
+        add(avatarPanel);
+
         backButton = new JButton("Back");
         backButton.addActionListener(e -> {
             goBackToMenu(previousPanel);
@@ -18,6 +29,12 @@ public class SettingsPanel extends JPanel {
 
         add(backButton);
 
+    }
+
+    private ImageIcon scaleAvatarPicture(String filename){
+        ImageIcon tempAvatar = new ImageIcon(filename);
+        Image scaledTempAvatar = tempAvatar.getImage().getScaledInstance(25,25,Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledTempAvatar);
     }
 
     private void goBackToMenu(JPanel previousPanel) {
