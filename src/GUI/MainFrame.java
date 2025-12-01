a842287858d44c9241aa0b273bc872f11fccf425
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2025-12-01 15:03:28 +0100

    Created the mainframe - wanna apply on the panels to avoid the problems we have atm

---
+++ src/GUI/MainFrame.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame(){
+        setTitle("QuizGame");
+        setSize(700, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
